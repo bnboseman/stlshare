b17e98a31e7633bb4821b75f5086f360eb659312
Nichole Boseman <nboseman@nyimac-dev3.local>
2017-04-25 16:06:21 -0400

    added endpoints to retrieve models by tag, to delete, to update, etc

--- app/stl/routes.js
+++ app/stl/routes.js
@@ -15,10 +15,32 @@ const storage = multer.diskStorage({
 
 let upload = multer({storage: storage}).array('stlfiles', 6);
 router.get('/', (request, response) => {
-  Stl.find().sort('-created').exec((err, stls)=> {
+  Stl
+    .find()
+    .sort('-created')
+    .lean()
+    .exec((error, stls)=> {
+    if(error) {
+      console.error(error);
+      return response.status(500).json({"error": error.message});
+    }
     return response.status(200).json(stls);
   });
 
+});
+
+router.get('/tag/:tag', (request, response) => {
+  Stl
+    .find()
+    .where('tags').in([request.params.tag])
+    .exec((error, stls) => {
+      if(error) {
+        console.error(error);
+        return response.status(500).json({"error": error.message});
+      }
+
+      return response.status(200).json(stls);
+    });
 });
 
 router.get('/:id', (request, response) => {
@@ -28,6 +50,7 @@ router.get('/:id', (request, response) => {
       return response.json(stl.toObject({versionKey: false}))
     });
 });
+
 
 router.post('/', (request, response) => {
   const required_fields = ['name'];
@@ -44,13 +67,36 @@ router.post('/', (request, response) => {
     description: request.body.description,
     owner: request.body.owner,
     category: request.body.category,
-    tags: request.body.tags.split(','),
+    tags: Stl.processTags(request.body.tags),
   }).then(Stl => response.status(201).json(Stl.toObject()))
     .catch(error => {
       console.log(error);
       response.status(500).json({error: 'Could not save STL'});
     });
+});
 
+router.delete('/:id', (request, response) => {
+  Stl
+    .findByIdAndRemove(request.params.id)
+    .exec()
+    .then(() => {
+      response.status(204).json({message: 'Success'});
+    })
+    .catch(error => {
+      console.error(error);
+      response.status(500).json({error: 'Could not delete model'});
+    });
+});
+
+router.put('/:id', (request, response) => {
+  if (!(request.params.id && request.body.id && request.params.id === request.body.id)) {
+    response.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updatableFields = ['name', 'description', 'pictures', 'tags'];
 });
 
 module.exports = router;
